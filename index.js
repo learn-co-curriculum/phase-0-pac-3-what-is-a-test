8b5d35613bebdf3589ca1e8a94ca50784c67034f
Liz Burton <liz.burton147@gmail.com>
2020-12-22 09:39:21 -0500

    reverts previous commit

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const name = "Joe";
 const height = 74;
 const message = `${name} is ${height} inches tall`;
 
-export default {name, height, message};
+module.exports = {name, height, message};
--- test/index-test.js
+++ test/index-test.js
@@ -1,26 +1,26 @@
-import { name, height, message } from "../index.js";
+const index = require("../index.js");
 
 /*
-
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
-      expect(name).toEqual('Susan')
+      expect(index.name).toEqual('Susan')
     })
   })
 
+
   describe('Height', () => {
     it('is less than 40', () => {
-      expect(height).toBeLessThan(40)
+      expect(index.height).toBeLessThan(40)
     })
   })
 
   describe('Message', () => {
     it('gives the name and height', () => {
-      expect(message).toInclude(name)
-      expect(message).toInclude(height)
+      expect(index.message).toInclude(index.name)
+      expect(index.message).toInclude(index.height)
     })
   })
 })
-
 */
+
