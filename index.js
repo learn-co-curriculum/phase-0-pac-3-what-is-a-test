009413a66ad648b767f6abc8aff992a839a08220
Liz Burton <liz.burton147@gmail.com>
2020-12-22 09:18:36 -0500

    update to es6, part 2

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// write your code below!
 const name = "Joe";
 const height = 74;
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+export default {name, height, message};
