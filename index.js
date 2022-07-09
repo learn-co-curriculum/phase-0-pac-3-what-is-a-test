7241d0150ce0bfd563ef875777d6261080452aa1
Matthew Dahl <sportinmd@me.com>
2022-07-09 13:21:23 -0700

    lea

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const name = "Joe";
+const name = "Susan";
 const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+console.log(message)
+
