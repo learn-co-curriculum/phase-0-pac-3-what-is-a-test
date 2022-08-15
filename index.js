ff63eeea72ac6c7c305da31a8494a65e2f71348e
kipkemoi98 <gilbert.kipkemoi@student.moringaschool.com>
2022-08-15 14:12:12 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+// const name = "Joe";
+// const height = 74;
+// const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+// module.exports = {name, height, message};
+
+    describe('Name', () => {
+      it('returns "Joe"', () => {
+        expect(name).toEqual('Joe')
+      })
+    })
+  
+  
+    describe('Height', () => {
+      it('is less than 74', () => {
+        expect(height).toBeLessThan(74)
+      })
+    })
+  
+    describe('Message', () => {
+      it('gives the name and height', () => {
+        expect(message).toInclude(name)
+        expect(message).toInclude(height)
+      })
+    })
